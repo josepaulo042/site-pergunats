60083f4fa8d1b1e7f9b39ed19085f2dc82af0006
josepaulo042 <137405026+josepaulo042@users.noreply.github.com>
2024-10-22 09:16:46 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,12 +1,12 @@
 criaCartao(
-    'programação',
-    'O que é HTML?',
-    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web'
+    'portugues',
+    'O que é um verbo?',
+    'é a palavra que expressa ações, estados e fenômenos da natureza'
 )
 criaCartao(
-    'Computador',
-    'O que é um viros?',
-    'Em informática, um vírus de computador é um software malicioso que é desenvolvido por programadores geralmente inescrupulosos'
+    'carro',
+    'O que é um pistão?',
+    'Eles se movem para cima e para baixo e são responsáveis por converter a energia da queima de combustível em trabalho mecânico para o virabrequim'
 )
 criaCartao(
     'futsal',
